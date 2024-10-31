0b3d5ff7cdefd9c895a412bbb60d902a0df85f66
Saiprasad Shivaji Patne <saipatne98@gmail.com>
2024-11-01 01:00:29 +0530

    React Component basics code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 export const App= () => {
-  return (
+  return( 
     <div>
-      <img src="attitude-black.png" alt="attitude-black.png" 
-       width="50%" height="40%" />
-      <h2>Name:Innova Hycross</h2>
-      <h3>Safety Ratings : 5 Star</h3>
-
-      <p>
-        Summary: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quaerat aliquam, pariatur sint deserunt amet voluptate suscipit veniam ullam ex praesentium numquam facilis dolore vel doloribus ratione ad perferendis maxime!
-      </p>
+          <InnovaHycross />   { /* Here the component called it will execute all the code in this component   */}
+          <InnovaHycross />
+          <InnovaHycross /> 
     </div>
   )
 };
 
 
+// COMPONENT defination 
+const InnovaHycross = () => { {/*Component name must be in PascalCasec */}
+  return(
+    <div>
+      <div>
+        <img src="attitude-black.png" alt="attitude-black.png" 
+        width="50%" height="40%" />
+        <h2>Name:Innova Hycross</h2>
+        <h3>Safety Ratings : 5 Star</h3>
 
+        <p>
+          Summary: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quaerat aliquam, pariatur sint deserunt amet voluptate suscipit veniam ullam ex praesentium numquam facilis dolore vel doloribus ratione ad perferendis maxime!
+        </p>
+      </div>
+    </div>
+  )
+}
