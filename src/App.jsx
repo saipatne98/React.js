fc12e9bcc253c707dd585c1562d33c439e64e829
Saiprasad Shivaji Patne <saipatne98@gmail.com>
2024-10-31 18:00:09 +0530

    all Files added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export const App= () => {
+  return (
+    <div>
+      <img src="attitude-black.png" alt="attitude-black.png" 
+       width="50%" height="40%" />
+      <h2>Name:Innova Hycross</h2>
+      <h3>Safety Ratings : 5 Star</h3>
+
+      <p>
+        Summary: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quaerat aliquam, pariatur sint deserunt amet voluptate suscipit veniam ullam ex praesentium numquam facilis dolore vel doloribus ratione ad perferendis maxime!
+      </p>
+    </div>
+  )
+};
+
+
+
