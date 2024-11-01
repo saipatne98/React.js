936d8560bf5800b139ef7e5b997669dfe7b2a66d
Saiprasad Shivaji Patne <saipatne98@gmail.com>
2024-11-02 01:15:13 +0530

    conditional Rendering learned

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,18 @@ const InnovaHycross = () => {
   let name = "Innova Hycross";
   let rating = 5;
   let summary = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quaerat aliquam, pariatur sint deserunt amet voluptate suscipit veniam ullam ex praesentium numquam facilis dolore vel doloribus ratione ad perferendis maxime!"
-
+  let age =19;
   const returnGenre=()=>{
     const genre ="RomCom";
     return genre;
   }
+
+  const canWatch =()=>{
+    if(age>=18){return "Watch Now";}
+    else{return "Not Available"}
+      
+  }
+
 
 
   return (
@@ -34,6 +41,10 @@ const InnovaHycross = () => {
         <h3>Safety Ratings : {rating} Star</h3>
         <p>Summary: {summary}</p>          {/*//variables can be used in JSX */}
         <p>Genre : {returnGenre()}</p>   {/* We can use fuctions as dynamically insert data*/}
+
+        <button>{canWatch()}</button>
+       
+        {/* <button>{age<18?"Not Available":"Watch Now"} </button>    COMMENT *Conditional Rendering  */}
       </div>
     </div>
   )
