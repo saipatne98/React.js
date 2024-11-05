18f65dc46272ebd5f4257aa49c34eabe3256936c
Saiprasad Shivaji Patne <saipatne98@gmail.com>
2024-11-05 06:27:01 +0530

    json and NetflixSeries Component Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import {NetflixSeries ,Footer} from "./components/NetflixSeries";
+import NetflixSeries  from "./components/NetflixSeries";
 
 export const App = () => {
   return (
     <>
       <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />  
-      <Footer/> { /* Here the component called it will execute all the code in this component   */}
+      
+       { /* Here the component called it will execute all the code in this component   */}
     </>  //react Fragment code syntax it removes one non required node 
   )
 };
--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,46 +1,31 @@
-
+import seriesData from "../api/SeriesData.json"
 // COMPONENT defination 
-export const NetflixSeries = () => {
+ const NetflixSeries = () => {
     {/*Component name must be in PascalCasec */ }
-  
-    let image = "attitude-black.png";
-    let name = "Innova Hycross";
-    let rating = 5;
-    let summary = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quaerat aliquam, pariatur sint deserunt amet voluptate suscipit veniam ullam ex praesentium numquam facilis dolore vel doloribus ratione ad perferendis maxime!"
-    let age =19;
-    const returnGenre=()=>{
-      const genre ="RomCom";
-      return genre;
-    }
-  
-    const canWatch =()=>{
-      if(age>=18){return "Watch Now";}
-      else{return "Not Available"}
-        
-    }
-  
   
   
     return (
-      <div>
+     <ul> 
+      {seriesData.map((currEle)=>{
+        return(
+      <li key={currEle.id}>   //key prop 
         <div>
-          <img src={image} alt="attitude-black.png" width="50%" height="40%" />
-          <h2>Name: {name} </h2>
-          <h3>Safety Ratings : {rating} Star</h3>
-          <p>Summary: {summary}</p>          {/*//variables can be used in JSX */}
-          <p>Genre : {returnGenre()}</p>   {/* We can use fuctions as dynamically insert data*/}
-  
-          <button>{canWatch()}</button>
-         
+          <img src={currEle.img_url} alt="attitude-black.png" width="50%" height="40%" />
+          <h2>Name: {currEle.name} </h2>
+          <h3>Safety Ratings : {currEle.rating} Star</h3>
+          <p>Summary: {currEle.description}</p>          {/*//variables can be used in JSX */}
+          <p>Genre : {currEle.genre}</p>   {/* We can use fuctions as dynamically insert data*/}
+          <a href={currEle.watch_url }target="_blank">
+          <button>Watch Now</button>
+          </a>
           {/* <button>{age<18?"Not Available":"Watch Now"} </button>    COMMENT *Conditional Rendering  */}
         </div>
-      </div>
+      </li>
+        )
+       
+      })}
+       </ul>
     )
-  };
+ }    
   
-
- export const Footer =()=>{
-    return(
-        <p>Copyright @SaiPatne2024</p>
-    )
-  };
+export default NetflixSeries;
